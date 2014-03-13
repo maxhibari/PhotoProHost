920cbff9e435363ecac0581f79375b6a9efc5a3f
Simon <simon.vlieghe@gmail.com>
2014-03-13 09:10:55 +0100

    base.sql
    base SQL, premier jet

---
+++ base.sql
@@ -0,0 +1,32 @@
+﻿DROP SCHEMA IF EXISTS imghoster CASCADE;
+CREATE SCHEMA imghoster;
+
+SET search_path = imghoster;
+
+CREATE TABLE users(
+	id serial primary key,
+	pseudo varchar(20) unique not null,
+	pass text not null,
+	email varchar(254) unique,
+	age int,
+	locked boolean default false
+);
+
+CREATE TABLE images(
+	id serial primary key,
+	link varchar(2083)
+);
+
+CREATE TABLE albums(
+	id serial primary key,
+	id_img int references images(id)
+);
+
+CREATE TABLE comments(
+	id serial primary key,
+	text varchar(300),
+	id_user int references users(id),
+	id_img int references images(id),
+	id_album int references albums(id),
+	id_comment int references comments(id)
+);
